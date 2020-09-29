3262800baf074136ffa7d3226855b2f0823b9de9
Franciscorp <franmedi_mdp@hotmail.com>
2020-09-29 11:56:32 -0300

    Updates

---
+++ precache-manifest.4f6c25237b6545c0f73d443af723fbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def2a3163dc80e0a5162654487c50de3",
+    "url": "/fmedina/index.html"
+  },
+  {
+    "revision": "6a7576b5f1e75239f8c5",
+    "url": "/fmedina/static/css/main.6d2c6673.chunk.css"
+  },
+  {
+    "revision": "a04e003de2c30b467b1a",
+    "url": "/fmedina/static/js/2.2dcc7a0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fmedina/static/js/2.2dcc7a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7576b5f1e75239f8c5",
+    "url": "/fmedina/static/js/main.5b3d4d0a.chunk.js"
+  },
+  {
+    "revision": "726edcef3f4a3a1e513f",
+    "url": "/fmedina/static/js/runtime-main.7a0bebf6.js"
+  }
+]);
