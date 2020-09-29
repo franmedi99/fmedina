8c1e364ce036b4847fadb54a64d815b2d9e1e36d
Franciscorp <franmedi_mdp@hotmail.com>
2020-09-29 13:04:38 -0300

    solution error

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
             <Route path="/Xp"><Xp></Xp></Route>
             <Route path="/start"><Inicio></Inicio></Route>
             <Route path="/fmedina"><Inicio></Inicio></Route>
+            <Route path="/fmedina/."><Inicio></Inicio></Route>
           </Switch>
         
     </div>
